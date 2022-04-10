e1d9a8c42b2d5d21d5da6c3595b77e1e9081b946
neil-dev <neil-dev@eyden.live>
2022-04-10 14:42:34 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # crud-http-frontend
 Simplest REST API Util (wrapper for axios)
+
+Example:
+```
+<div id="reponse"></div>
+
+<script src="axios.min.js"></script>
+<script src="crud-http.js"></script>
+<script>
+    let url = 'http://localhost:5684/api'; // replace with your API
+    let promise = http_read(url);
+    promise.then((data) => {
+        let element = document.getElementById("reponse");
+        element.innerHTML = JSON.stringify(data);
+    });
+      
+    // optional error handling
+    promise.catch((err) =>{
+        let element = document.getElementById("reponse");
+        element.innerHTML = JSON.stringify(err);
+    });
+</script>
+```
+
+#### Methods
+```
+let promise = http_create(url,data,authValue=null); // authValue sets "Authorization" header for account specific actions
+let promise = http_read(url,authValue=null); // authValue sets "Authorization" header for account specific actions
+let promise = http_update(url,data,authValue=null); // authValue sets "Authorization" header for account specific actions
+let promise = http_delete(url,authValue=null); // authValue sets "Authorization" header for account specific actions
+``` 
+
+
---
+++ crud-http.js
@@ -0,0 +1,158 @@
+async function http_create(url,data,authValue=null) {
+  let o = '';
+  try { 
+    if(authValue!==null) {
+           o = await postWithAuth(url,data,authValue);
+    } else {
+          o = await post(url,data);
+    }
+  } catch(err) {
+    if(String(err).includes("REFUSED")) {
+       o = resultToStrOrJsonError({status:404,data:String(err)})
+     } else {
+      	throw err; 
+     }
+  }
+  
+  return o;
+}
+
+
+// simple POST request 
+async function post(url,obj) {
+    let r = await instance.post(url,obj);
+  	return resultToStrOrJsonError(r);
+}
+
+// simple POST request with Authorization header
+async function postWithAuth(url,obj,authValue) {
+    const config = { headers: {"Authorization":authValue}};
+    let r = await instance.post(url,obj,config);
+  	return resultToStrOrJsonError(r);
+}
+
+
+
+
+
+async function http_read(url,authValue=null) {
+  let o = '';
+  try { 
+    if(authValue!==null) {
+           o = await getWithAuth(url,authValue);
+    } else {
+          o = await get(url);
+    }
+  } catch(err) {
+    if(String(err).includes("REFUSED")) {
+       o = resultToStrOrJsonError({status:404,data:String(err)})
+     }  else {
+      	throw err; 
+     }
+  }
+  
+  return o;
+}
+
+// simple GET request
+async function get(url) {
+    let r = await instance.get(url);
+  	return resultToStrOrJsonError(r);
+}
+
+// simple GET request with Authorization header
+async function getWithAuth(url,authValue) {
+    const config = { headers: {"Authorization":authValue}};
+    let r = await instance.get(url,config);
+  	return resultToStrOrJsonError(r);
+}
+
+
+
+
+                   
+async function http_update(url,data,authValue=null) {
+  let o = '';
+  try { 
+    if(authValue!==null) {
+           o = await putWithAuth(url,data,authValue);
+    } else {
+          o = await put(url,data);
+    }
+  } catch(err) {
+    if(String(err).includes("REFUSED")) {
+       o = resultToStrOrJsonError({status:404,data:String(err)})
+     }  else {
+      	throw err; 
+     }
+  }
+  
+  return o;
+}
+
+
+// simple put request 
+async function put(url,obj) {
+    let r = await instance.put(url,obj);
+  	return resultToStrOrJsonError(r);
+}
+
+// simple put request with Authorization header
+async function putWithAuth(url,obj,authValue) {
+    const config = { headers: {"Authorization":authValue}};
+    let r = await instance.put(url,obj,config);
+  	return resultToStrOrJsonError(r);
+}
+
+
+
+
+
+async function http_delete(url,authValue=null) {
+  let o = '';
+  try { 
+    if(authValue!==null) {
+           o = await deleteWithAuth(url,authValue);
+    } else {
+          o = await deletesinglefn(url);
+    }
+  } catch(err) {
+    if(String(err).includes("REFUSED")) {
+       o = resultToStrOrJsonError({status:404,data:String(err)})
+     }  else {
+      	throw err; 
+     }
+  }
+  
+  return o;
+}
+
+// simple delete request
+async function deletesinglefn(url) {
+    let r = await instance.delete(url);
+  	return resultToStrOrJsonError(r);
+}
+
+// simple delete request with Authorization header
+async function deleteWithAuth(url,authValue) {
+    const config = { headers: {"Authorization":authValue}};
+    let r = await instance.delete(url,config);
+  	return resultToStrOrJsonError(r);
+}
+
+
+
+// prevent non 200-299 status codes like 404 from throwing js error
+const instance = axios.create({
+    validateStatus: function validateStatus(status) {
+        return true;
+    }
+});
+
+function resultToStrOrJsonError(r){
+ if(r.status < 200 || r.status > 299) {
+     	return {"status":r.status,"error":r.data} 
+    } else {
+    	return r.data;
+    } 
+}
